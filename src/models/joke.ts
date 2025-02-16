df7b52f189236f6a5bf367ac8eeec87f860b35a9
Serhii Kushnir <s1983kushnir@gmail.com>
2025-02-16 18:25:53 +0100

    Added model mongoose Schema Joke

---
+++ src/models/joke.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface IVote {
+  value: number;
+  label: string;
+}
+
+export interface IJoke extends Document {
+  question: string;
+  answer: string;
+  votes: IVote[];
+  availableVotes: string[];
+}
+
+const JokeSchema: Schema = new Schema({
+  question: { type: String, required: true },
+  answer: { type: String, required: true },
+  votes: [
+    {
+      value: Number,
+      label: String,
+    },
+  ],
+  availableVotes: [String],
+});
+
+export default mongoose.model<IJoke>("Joke", JokeSchema);
